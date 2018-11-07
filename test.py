419f095aed2ef607cc7a2d37e30398dd26f6ee1d
Paolo1984 <43854520+Paolo1984@users.noreply.github.com>
2018-11-07 12:08:04 +0100

    Update test.py
    Dictionary test

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("Hello world!")
+
+phone_book = {
+                'head': ['328-45684635', 'red.man@bt.com'], 
+                 'rap': ['347-987645', 'rap@ste.com'],
+                'mouse': ['335-6958475', 'gatto@cat.com']
+                }
+
+print(phone_book['testa'])
+
+#DICT['testa'] --> list of thing containing phone and mail
+print(phone_book['head'][0])
+print(phone_book['rap'][0])
+print(phone_book['mouse'][1])
